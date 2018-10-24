c3ba5bf0403ca78305c80f4c195dd28dce802f04
Colin But <colinbut@users.noreply.github.com>
2018-10-24 23:10:40 +0100

    initial project template from start.spring.io

---
+++ src/main/java/com/mycompany/microservicestemplate/MicroservicesTemplateApplication.java
@@ -0,0 +1,12 @@
+package com.mycompany.microservicestemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesTemplateApplication.class, args);
+    }
+}
